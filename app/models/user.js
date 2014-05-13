3ca96ecee21a02160fff9b9ae41927bd2a8d34ec
Sean <dokko1230@gmail.com>
2014-05-12 17:12:44 -0700

    adds recent updates

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,18 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  initialize: function(){
+    this.on('creating', function(model, attrs, options) {
+      //hash the password here
+    });
+  }
 });
+
+
+
+//if a user exists inside users,
+//allow user to see links
 
 module.exports = User;
