be97bca3df5ed9a22ad655e5081f2ae54106365e
Sean <dokko1230@gmail.com>
2014-05-12 17:10:17 -0700

    adds salting and username lookup

--- app/collections/users.js
+++ app/collections/users.js
@@ -1,8 +1,35 @@
 var db = require('../config');
 var User = require('../models/user');
+var bcrypt = require('bcrypt');
 
 var Users = new db.Collection();
 
 Users.model = User;
 
+Users.hasUserName = function(name, callback){
+//checks if name exists
+  Users.query().where({username: name}).select()
+    .then(function(response){
+      // console.log('response: ', response);
+      callback(response);
+    });
+};
+
+Users.hashPassword = function(password, callback){
+  bcrypt.genSalt(10, function(err, salt){
+    bcrypt.hash(password, salt, function(err, hash){
+      console.log('hash: ', hash);
+      callback(hash);
+    });
+  });
+};
+
+Users.checkPassword = function(username, hash, callback){
+  //check if password matches at username
+  //return true/false accrodingly
+  // Users
+};
+
 module.exports = Users;
+
+
